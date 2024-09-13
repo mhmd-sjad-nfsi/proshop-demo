dd0c3213826d53eb1e4023e07a87c9a24ebb5385
mhmd-sjad-nfsi <mhmdsjadnfysy@gmail.com>
2024-09-13 07:39:46 +0330

    front 2-4-4

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,14 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import { Container } from "react-bootstrap";
+import HomeScreen from "./screens/HomeScreen";
 const App = () => {
   return(
     <>
       <Header />
       <main className="py-3">
         <Container>
-        <h1>welcome to proshop</h1>;
+            <HomeScreen/>;
         </Container>
       </main>
       <Footer  />
