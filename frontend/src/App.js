4ef50559db1c0f12b7fe5843ebbfb010ff79e0c3
mhmd-sjad-nfsi <mhmdsjadnfysy@gmail.com>
2024-09-13 23:00:15 +0330

    front2 -5

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,14 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import { Container } from "react-bootstrap";
-import HomeScreen from "./screens/HomeScreen";
+import { Outlet } from "react-router-dom";
 const App = () => {
   return(
     <>
       <Header />
       <main className="py-3">
         <Container>
-            <HomeScreen/>;
+            <Outlet/>;
         </Container>
       </main>
       <Footer  />
