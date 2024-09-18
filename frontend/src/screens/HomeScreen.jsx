0113b1ca66392acaf76eb654619a822634d9d1ff
mhmd-sjad-nfsi <mhmdsjadnfysy@gmail.com>
2024-09-18 16:48:38 +0330

    server 3

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,8 +1,19 @@
-import {Row,Col} from 'react-bootstrap'
-import products from '../products'
-import { Product } from '../components/Product'
+import { useEffect,useState } from 'react';
+import {Row,Col} from 'react-bootstrap';
+import Product from '../components/Product';
+import axios from "axios"
+
 
 const HomeScreen = () => {
+  const [products,setProducts] =useState([]);
+
+  useEffect(() =>{
+    const fetchProducts=async() =>{
+      const {data}= await axios.get('/api/product');
+      setProducts(data);
+    };
+    fetchProducts();
+  },[])
   return (
     <>
     <h1>Latest Products</h1>
