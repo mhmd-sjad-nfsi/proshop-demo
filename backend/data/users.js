436ba0ccced0597a9b5693b262a21a1d8ddde998
mhmd-sjad-nfsi <mhmdsjadnfysy@gmail.com>
2024-09-19 17:42:40 +0330

    mongo 5

---
+++ backend/data/users.js
@@ -0,0 +1,27 @@
+import bcrypt from 'bcryptjs';
+
+const users =[
+    {
+        name:'Admin Users',
+        email:'admin@email.com',
+        password:bcrypt.hashSync('123456',10),
+        isAdmin:true,
+        
+    },
+    {
+        name:'John Doe',
+        email:'john@email.com',
+        password:bcrypt.hashSync('123456',10),
+        isAdmin:false,
+
+    },
+    {
+        name:'Jane Doe',
+        email:'jane@email.com',
+        password:bcrypt.hashSync('123456',10),
+        isAdmin:false,
+
+    }
+];
+
+export default users;
